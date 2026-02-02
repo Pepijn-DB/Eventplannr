bf3df4d95cd34d7bccc0240686b20405a35f0433
Pepijn <pepijn@axyvon.com>
2026-02-02 01:37:27 +0100

    Moved .env-template and config.php to front-end/ also created env function in config

---
+++ front-end/config.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+$env = file_get_contents(__DIR__."/.env");
+$lines = explode("\n",$env);
+
+foreach($lines as $line){
+    preg_match("/([^#]+)=(.*)/",$line,$matches);
+    if(isset($matches[2])){ putenv(trim($line)); }
+}
+
+function env(string $key, $default = null) {
+    if (!empty(getenv($key))) {
+        $value = getenv($key);
+        if ((str_starts_with($value, '"') && str_ends_with($value, '"'))
+            || (str_starts_with($value, "'") && str_ends_with($value, "'"))
+            || (str_starts_with($value, "`") && str_ends_with($value, "`"))) {
+            return substr($value, 1, -1);
+        } else if (is_numeric($value)) {
+            return (float) $value;
+        }
+    }
+
+    return $default;
+}
+
+$DB_SERVER = (string) env('DB_SERVER', 'localhost');
+$DB_USER = (string) env('DB_USER', 'user');
+$DB_PASSWORD = (string) env('DB_PASSWORD', 'pass');
+$DB_DATABASE = (string) env('DB_DATABASE', 'database');
+
+$WEBSITE_NAME = (string) env('WEBSITE_NAME', 'Eventplannr');
+
+$DISCORD_SECRET = (string) env('DISCORD_SECRET', 'secret');
+$DISCORD_ID = (string) env('DISCORD_ID', 'id');
+
