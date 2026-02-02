8072b78dc37771dd44aa6dd6240aeded84b37239
Pepijn <pepijn@axyvon.com>
2026-02-03 00:38:36 +0100

    Add routes for event and user, also use AuthRequest in 404

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 import authRoutes from './routes/v1/authRoutes.js';
+import userRoutes from './routes/v1/userRoutes.js';
+import eventRoutes from './routes/v1/eventRoutes.js';
+
 import { checkToken } from "./middlewares/v1/authHandler.js";
 import { errorHandler } from './middlewares/errorHandler.js';
+
+import type {JwtPayload} from "jsonwebtoken";
+import type {Request} from "express";
+
 export interface AuthRequest extends Request {
     user?: JwtPayload | string
 }
@@ -14,10 +21,11 @@ app.use(checkToken);
 
 // Routes V1
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/event', eventRoutes)
 
 app.use(errorHandler);
 
-app.use(function(req,res){
 app.use(function(req: AuthRequest,res){
     res.status(404).json({
         'message': 'Route not found'
