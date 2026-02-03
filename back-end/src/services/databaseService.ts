8251a96ec07e15a5babbecd5c3107464ff00b5e8
Pepijn <pepijn@axyvon.com>
2026-02-04 00:47:48 +0100

    Add return types to connect() and query(), to avoid using any. connect now return a (maybepromise) queryresult (postgres), query (mysql) or null (edge cases)

--- back-end/src/services/databaseService.ts
+++ back-end/src/services/databaseService.ts
@@ -2,8 +2,12 @@ import { dbConfig } from '../config/config.js';
 import postgres from './postgresService.js';
 import mysql from './mysqlService.js';
 
+import type {QueryResult} from "pg";
+import type {Query} from "mysql2";
 
-export function connect():boolean {
+import type {MaybePromise} from "../models/maybepromise.js";
+
+export function connect():MaybePromise<boolean> {
     switch (dbConfig.type) {
         case 'mysql': {
             return mysql.connect();
@@ -15,17 +19,18 @@ export function connect():boolean {
     }
 }
 
-export function query(query: string, params: any[] = []):any {
+export function query(query: string, params: any[] = []):MaybePromise<QueryResult> | Query | null {
     switch (dbConfig.type) {
         case 'mysql': {
-            if (!mysql.connect()) {return {error: "Database connection failed"};}
+            if (!mysql.connect()) {return null;}
             return mysql.query(query, params);
         }
         case 'postgres': {
-            if (!postgres.connect()) {return {error: "Database connection failed"};}
+            if (!postgres.connect()) {return null;}
             return postgres.query(query, params);
         }
     }
+    return null;
 }
 
 export default { connect, query };
