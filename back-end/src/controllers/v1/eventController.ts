c2d94e46caeee4c6b3bb589eae134ac1f02ab6ea
Pepijn <pepijn@axyvon.com>
2026-02-05 16:11:49 +0100

    Removed unused import (Request)

--- back-end/src/controllers/v1/eventController.ts
+++ back-end/src/controllers/v1/eventController.ts
@@ -1,4 +1,4 @@
-import type {Request, Response, NextFunction} from 'express';
+import type { Response, NextFunction} from 'express';
 import type {AuthRequest} from "../../app.js";
 
 export const getEvents = (req: AuthRequest, res: Response, next: NextFunction) => {
