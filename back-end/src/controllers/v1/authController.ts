9032ed3548dac5833c65ae844dccc8d791e80048
Pepijn <pepijn@axyvon.com>
2026-02-05 20:19:24 +0100

    Create the logic for authentication users

--- back-end/src/controllers/v1/authController.ts
+++ back-end/src/controllers/v1/authController.ts
@@ -2,6 +2,8 @@ import type {Response, NextFunction} from 'express';
 import type {AuthRequest} from "../../app.js";
 import type {User} from "../../models/user.js";
 
+import {queryWithoutExecutioner} from "../../services/databaseService.js";
+
 import {AppError} from "../../middlewares/errorHandler.js";
 
 import jwt from 'jsonwebtoken';
@@ -9,7 +11,7 @@ import Config from "../../config/config.js";
 import {emailValidator} from "../../validators/emailValidator.js";
 import {arrayValidator, variableValidator} from "../../validators/variableValidator.js";
 
-export const getToken = (req: AuthRequest, res: Response, next: NextFunction) => {
+export const getToken = async (req: AuthRequest, res: Response, next: NextFunction) => {
     try {
         if (Config.secret === 'null') {
             next(new AppError("Server not configured", 500));
@@ -33,14 +35,14 @@ export const getToken = (req: AuthRequest, res: Response, next: NextFunction) =>
             return res.status(400).json({message: "Invalid email"});
         }
 
-        //TODO Authenticate user
-        const userId = 1;
-        const username = "test";
+        const queryResult = await queryWithoutExecutioner('SELECT * FROM users WHERE email = ? AND password = ?', [email, password]);
+        if (queryResult === null) throw new AppError("Unauthorized", 401);
+        const user = queryResult.rows[0];
 
         const payload: User = {
-            id: userId,
-            username: username,
-            email: email,
+            id: user.id,
+            username: user.username,
+            email: user.email,
         }
 
         const token = jwt.sign(payload, Config.secret, {
