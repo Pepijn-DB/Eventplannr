d9133eba0424d0d04b56cc5090dd41366b8de19c
Pepijn <pepijn@axyvon.com>
2026-02-07 23:12:22 +0100

    Created getInvitations, added a checker if the user has permissions to view the event (is either invited or the organizer)

--- back-end/src/controllers/v1/invitationController.ts
+++ back-end/src/controllers/v1/invitationController.ts
@@ -1,8 +1,47 @@
 import type { Response, NextFunction} from 'express';
 import type {AuthRequest} from "../../app.js";
+import databaseService from "../../services/databaseService.js";
+import {userValidator, eventValidator} from "../../validators/requestValidator.js";
+import {AppError} from "../../middlewares/errorHandler.js";
 
-export const getInvitations = (req: AuthRequest, res: Response, next: NextFunction) => {
-    const eventId = req.params.id;
+async function hasEventViewPermission(eventId: number, userId: number): Promise<boolean> {
+    const sqlInvitations = `
+       SELECT i.*
+       FROM invitation i
+       WHERE i.event_id = ? AND i.user_id = ?
+     `;
+
+    const sqlEvent = `
+        SELECT e.*
+        FROM events e
+        WHERE e.event_id = ? AND e.creator_user = ?
+    `;
+
+    const resultInvitation = await databaseService.query(sqlInvitations, [eventId], userId);
+    const resultEvent = await databaseService.query(sqlEvent, [eventId], userId)
+
+    return resultInvitation.rows.length > 0 || resultEvent.rows.length > 0;
+}
+
+export const getInvitations = async (req: AuthRequest, res: Response, next: NextFunction) => {
+    try {
+        const userId = userValidator(req);
+        const eventId = eventValidator(req);
+
+        if (!(await hasEventViewPermission(eventId, userId))){
+            return res.status(403).json({message: "Forbidden"});
+        }
+        const sql = `
+            SELECT i.*
+            FROM invitation i
+            WHERE i.event_id = ?
+        `;
+        const result = await databaseService.query(sql, [eventId], userId);
+        if (!result) throw new AppError("Internal server error");
+        return res.status(200).json(result.rows);
+    } catch (err) {
+        next(err);
+    }
 }
 
 export const deleteInvitation = (req: AuthRequest, res: Response, next: NextFunction) => {
