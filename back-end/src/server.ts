4afdbe2f5df5cd9891f19e6bdeef9b6ce4991872
Pepijn <pepijn@axyvon.com>
2026-02-09 23:57:36 +0100

    Connect to the db server in server.ts

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,10 @@
 import app from "./app.js";
 import config from "./config/config.js";
 
+import database from "./services/databaseService.js";
+
+database.connect();
+
 app.listen(config.port, () => {
     console.log(`Server running on port ${config.port}`);
 });
