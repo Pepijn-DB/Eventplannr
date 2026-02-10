a35461435246ad7f424f8fa332fe0f357537c874
Pepijn <pepijn@axyvon.com>
2026-02-10 16:24:22 +0100

    Console log when connected to the database

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -3,7 +3,7 @@ import config from "./config/config.js";
 
 import database from "./services/databaseService.js";
 
-database.connect();
+database.connect().then(r => console.log("Connected to database", r));
 
 app.listen(config.port, () => {
 	console.log(`Server running on port ${config.port}`);
