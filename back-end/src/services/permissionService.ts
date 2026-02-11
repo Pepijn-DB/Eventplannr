c0d41353ba98c2e97271ec0f66057e15271a5f51
Pepijn <pepijn@axyvon.com>
2026-02-11 01:02:15 +0100

    Init of splitting checking user permissions into a different file, made a enum and created the permissionService file

---
+++ back-end/src/services/permissionService.ts
@@ -0,0 +1,8 @@
+// import type {Event, Global} from "../models/permissions.js";
+// import database from "../services/databaseService.js"
+
+// export function hasEventPermission(user:number, permission:Event) {
+//     switch (permission) {
+            
+//     }
+// }
