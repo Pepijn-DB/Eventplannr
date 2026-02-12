1993b8b2a89d686c980a47899418595aeecb486b
Pepijn <pepijn@axyvon.com>
2026-02-12 13:25:11 +0100

    Added event param to check if user has permission on event

--- back-end/src/services/permissionService.ts
+++ back-end/src/services/permissionService.ts
@@ -3,57 +3,56 @@ import database from "../services/databaseService.js";
 
 export async function hasEventPermission(
 	user: number,
+	event: number,
 	permission: Event,
 ): Promise<boolean> {
 	switch (permission) {
 		case Event.VIEW: {
-			const sqlInv = `SELECT i.user_id, i.event_id, i.role FROM invitations i WHERE i.user_id = ?`;
-			const sqlEvent = `SELECT e.id, e.creator_id FROM events e WHERE e.creator_id = ?`;
-			const resultInv = await database.query(sqlInv, [user], user);
-			const resultEvent = await database.query(sqlEvent, [user], user);
+			const sqlInv = `SELECT i.user_id, i.event_id, i.role FROM invitations i WHERE i.user_id = ? AND i.event_id = ?`;
+			const resultInv = await database.query(sqlInv, [user, event], user);
 			return (
-				resultEvent.rows.length > 0 ||
 				resultInv.rows.length > 0 ||
-				(await hasGlobalPermission(user, Global.VIEW_ALL_EVENTS))
+				(await hasGlobalPermission(user, Global.VIEW_ALL_EVENTS)) ||
+				(await hasEventPermission(user, event, Event.EDIT_ALL))
 			);
 		}
 		case Event.EDIT_DETAILS: {
-			return hasEventPermission(user, Event.EDIT_ALL);
+			return hasEventPermission(user, event, Event.EDIT_ALL);
 		}
 		case Event.EDIT_LOCATION: {
-			const sqlInv = `SELECT i.user_id, i.event_id, i.role FROM invitations i WHERE i.user_id = ? AND (i.role = ORGANIZER OR i.role = LOCATION_PICKER)`;
-			const resultInv = await database.query(sqlInv, [user], user);
+			const sqlInv = `SELECT i.user_id, i.event_id, i.role FROM invitations i WHERE i.user_id = ? AND i.event_id = ? AND (i.role = ORGANIZER OR i.role = LOCATION_PICKER)`;
+			const resultInv = await database.query(sqlInv, [user, event], user);
 			return (
 				resultInv.rows.length > 0 ||
 				(await hasGlobalPermission(user, Global.EDIT_ALL_EVENTS)) ||
-				(await hasEventPermission(user, Event.EDIT_ALL))
+				(await hasEventPermission(user, event, Event.EDIT_ALL))
 			);
 		}
 		case Event.EDIT_INVITATION: {
 			{
-				const sqlInv = `SELECT i.user_id, i.event_id, i.role FROM invitations i WHERE i.user_id = ? AND i.role = ORGANIZER`;
-				const resultInv = await database.query(sqlInv, [user], user);
+				const sqlInv = `SELECT i.user_id, i.event_id, i.role FROM invitations i WHERE i.user_id = ? AND i.event_id = ? AND i.role = ORGANIZER`;
+				const resultInv = await database.query(sqlInv, [user, event], user);
 				return (
 					resultInv.rows.length > 0 ||
 					(await hasGlobalPermission(user, Global.EDIT_ALL_INVITATIONS)) ||
-					(await hasEventPermission(user, Event.EDIT_ALL))
+					(await hasEventPermission(user, event, Event.EDIT_ALL))
 				);
 			}
 		}
 		case Event.EDIT_DATE: {
-			const sql = `SELECT i.user_id, i.event_id, i.role FROM invitations i WHERE i.user_id = ? AND (i.role = DATE_PICKER or i.role = ORGANIZER)`;
-			const result = await database.query(sql, [user], user);
+			const sql = `SELECT i.user_id, i.event_id, i.role FROM invitations i WHERE i.user_id = ? AND i.event_id = ? AND (i.role = DATE_PICKER or i.role = ORGANIZER)`;
+			const result = await database.query(sql, [user, event], user);
 			return (
 				result.rows.length > 0 ||
 				(await hasGlobalPermission(user, Global.EDIT_ALL_EVENTS)) ||
-				(await hasEventPermission(user, Event.EDIT_ALL))
+				(await hasEventPermission(user, event, Event.EDIT_ALL))
 			);
 		}
 		case Event.EDIT_ALL: {
-			const sqlInv = `SELECT i.user_id, i.event_id, i.role FROM invitations i WHERE i.user_id = ? AND i.role = ORGANIZER`;
-			const sqlEvent = `SELECT e.id, e.creator_id FROM events e WHERE e.creator_id = ?`;
-			const resultInv = await database.query(sqlInv, [user], user);
-			const resultEvent = await database.query(sqlEvent, [user], user);
+			const sqlInv = `SELECT i.user_id, i.event_id, i.role FROM invitations i WHERE i.user_id = ? AND i.event_id = ? AND i.role = ORGANIZER`;
+			const sqlEvent = `SELECT e.id, e.creator_id FROM events e WHERE e.creator_id = ? AND e.id = ?`;
+			const resultInv = await database.query(sqlInv, [user, event], user);
+			const resultEvent = await database.query(sqlEvent, [user, event], user);
 			return (
 				resultEvent.rows.length > 0 ||
 				resultInv.rows.length > 0 ||
