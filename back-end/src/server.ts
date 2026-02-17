0cb4d6510585ea6215a0c0fbc7cf3aa5a891f340
Pepijn <pepijn@axyvon.com>
2026-02-18 00:41:39 +0100

    If the secret is not set, dont start

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -5,6 +5,11 @@ import database from "./services/databaseService.js";
 
 database.connect().then((r) => console.log("Connected to database", r));
 
+if (config.secret === "SECRET STRING SHOULD BE SET IN ENV") {
+	console.error("Secret not set in env");
+	process.exit(1);
+}
+
 app.listen(config.port, () => {
 	console.log(`Server running on port ${config.port}`);
 });
