157c4ff98b64079cf414ba2e98c140616ffa8ef6
Pepijn <pepijn@axyvon.com>
2026-02-18 11:29:07 +0100

    Connect to DB after checking secret string

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -3,12 +3,12 @@ import config from "./config/config.js";
 
 import database from "./services/databaseService.js";
 
-database.connect().then((r) => console.log("Connected to database", r));
-
 if (config.secret === "SECRET STRING SHOULD BE SET IN ENV") {
 	console.error("Secret not set in env");
 	process.exit(1);
 }
+
+database.connect().then((r) => console.log("Connected to database", r));
 
 app.listen(config.port, () => {
 	console.log(`Server running on port ${config.port}`);
