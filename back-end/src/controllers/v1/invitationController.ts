9b6bc1825f8cbbc6260aad5077db26d4ba06f07d
Pepijn <pepijn@axyvon.com>
2026-03-21 01:00:22 +0100

    Add param , the requestedUser

--- back-end/src/controllers/v1/invitationController.ts
+++ back-end/src/controllers/v1/invitationController.ts
@@ -238,6 +238,7 @@ export const getInvitation = async (
 ) => {
 	try {
 		const { userId, eventId } = getRequestVariables(req, false);
+		const requestedUser = (variableValidator(req.body.user_id) ? req.body.user_id : null) as StrNum;
 
 		if (!(await hasEventPermission(userId, eventId, Event.VIEW))) {
 			return res.status(403).json({ message: "Forbidden" });
@@ -248,7 +249,7 @@ export const getInvitation = async (
         WHERE i.event_id = ? AND i.user_id = ?
     `;
 
-		const result = await databaseService.query(sql, [eventId], userId);
+		const result = await databaseService.query(sql, [eventId, requestedUser], userId);
 		if (!result) {
 			return res.status(500).json({ message: "Internal server error" });
 		}
