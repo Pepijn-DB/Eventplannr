ce9610ba4d0e1bc416a998b8bc7fd1b0c3ed64e4
Pepijn <pepijn@axyvon.com>
2026-04-01 10:02:49 +0200

    Update vitest to add coverage

--- back-end/vitest.config.ts
+++ back-end/vitest.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
 		globals: true,
 		environment: "node",
 		passWithNoTests: false,
+		coverage: {
+			provider: "v8",
+		},
 		include: ["tests/**/*.test.ts"],
 	},
 });
