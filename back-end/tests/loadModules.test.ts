60b908736d8a0cea540bd07987b8faf3dce283ca
Pepijn <pepijn@axyvon.com>
2026-04-17 11:38:53 +0200

    Lint fix

--- back-end/tests/loadModules.test.ts
+++ back-end/tests/loadModules.test.ts
@@ -1,43 +1,44 @@
-import { describe, it, expect } from 'vitest';
+/** biome-ignore-all lint/suspicious/noExplicitAny: <Tests need to have any to use methods as any> */
+import { describe, expect, it } from "vitest";
 
 // This test ensures that each module can be imported without throwing.
 // It is intentionally light-weight: it only checks module loadability so we can
 // get baseline coverage for modules that currently lack focused unit tests.
 
 const MODULES = [
-	'../../src/models/hash',
-	'../../src/models/location',
-	'../../src/models/event',
-	'../../src/models/maybepromise',
-	'../../src/models/user',
-	'../../src/models/strnum',
-	'../../src/models/sqlTables',
-	'../../src/models/permissions',
-	'../../src/validators/variableValidator',
-	'../../src/validators/requestValidator',
-	'../../src/validators/emailValidator',
-	'../../src/middlewares/errorHandler',
-	'../../src/middlewares/v1/authHandler',
-	'../../src/middlewares/v1/rateLimitHandler',
-	'../../src/app',
-	'../../src/config/config',
-	'../../src/routes/v1/userRoutes',
-	'../../src/routes/v1/responseRoutes',
-	'../../src/routes/v1/locationRoutes',
-	'../../src/routes/v1/eventRoutes',
-	'../../src/routes/v1/authRoutes',
-	'../../src/routes/v1/adminRoutes',
-	'../../src/controllers/v1/userController',
-	'../../src/controllers/v1/responseController',
-	'../../src/controllers/v1/locationController',
-	'../../src/controllers/v1/invitationController',
-	'../../src/controllers/v1/eventController',
-	'../../src/controllers/v1/dateController',
-	'../../src/controllers/v1/authController',
-	'../../src/controllers/v1/adminController',
+	"../../src/models/hash",
+	"../../src/models/location",
+	"../../src/models/event",
+	"../../src/models/maybepromise",
+	"../../src/models/user",
+	"../../src/models/strnum",
+	"../../src/models/sqlTables",
+	"../../src/models/permissions",
+	"../../src/validators/variableValidator",
+	"../../src/validators/requestValidator",
+	"../../src/validators/emailValidator",
+	"../../src/middlewares/errorHandler",
+	"../../src/middlewares/v1/authHandler",
+	"../../src/middlewares/v1/rateLimitHandler",
+	"../../src/app",
+	"../../src/config/config",
+	"../../src/routes/v1/userRoutes",
+	"../../src/routes/v1/responseRoutes",
+	"../../src/routes/v1/locationRoutes",
+	"../../src/routes/v1/eventRoutes",
+	"../../src/routes/v1/authRoutes",
+	"../../src/routes/v1/adminRoutes",
+	"../../src/controllers/v1/userController",
+	"../../src/controllers/v1/responseController",
+	"../../src/controllers/v1/locationController",
+	"../../src/controllers/v1/invitationController",
+	"../../src/controllers/v1/eventController",
+	"../../src/controllers/v1/dateController",
+	"../../src/controllers/v1/authController",
+	"../../src/controllers/v1/adminController",
 ];
 
-describe('module load smoke tests', () => {
+describe("module load smoke tests", () => {
 	for (const m of MODULES) {
 		it(`imports ${m}`, async () => {
 			const mod = await import(m);
@@ -45,5 +46,3 @@ describe('module load smoke tests', () => {
 		});
 	}
 });
-
-
