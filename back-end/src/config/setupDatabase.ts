1bf6c18892568b25499d4f6fa44c9b8f9cbef749
Pepijn <pepijn@axyvon.com>
2026-04-22 11:28:14 +0200

    Create helper function to get db commands from file and excecute them

--- back-end/src/config/setupDatabase.ts
+++ back-end/src/config/setupDatabase.ts
@@ -12,4 +12,21 @@ export async function setupDatabase():Promise<boolean> {
         return false;
     }
     throw new Error("Unsupported database type");
+async function setupSql(filePath: string): Promise<void> {
+    await queryWithoutExecutioner(`CREATE DATABASE ${dbConfig.database}`);
+    fs.readFile(filePath, "utf-8", async (err, data) => {
+        if (err) {
+            console.error("Error reading schema.sql:", err);
+            return false;
+        }
+        const queries = data.split(";").map(q => q.trim()).filter(q => q.length > 0);
+        for (const query of queries) {
+            try {
+                await queryWithoutExecutioner(query);
+            } catch (err) {
+                console.error("Error executing query:", query, err);
+                return false;
+            }
+        }
+    });
 }
