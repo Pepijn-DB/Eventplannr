d1c466452aa5fbe52d10385456cbd0ddd8bd1756
Pepijn <pepijn@axyvon.com>
2026-04-22 11:31:35 +0200

    Implement setting up the database

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -3,12 +3,19 @@ import config from "./config/config.js";
 
 import database from "./services/databaseService.js";
 
+import {setupDatabase} from "./config/setupDatabase.js";
+
 if (config.secret === "SECRET STRING SHOULD BE SET IN ENV") {
 	console.error("\x1b[31m[ERROR] Secret not set in env\x1b[0m");
 	process.exit(1);
 }
 
 database.connect().then((r) => console.log("Connected to database", r));
+
+setupDatabase().then((r) => {
+	if(r == null) return;
+	console.log("Database setup", r);
+});
 
 app.listen(config.port, () => {
 	console.log(`Server running on port ${config.port}`);
