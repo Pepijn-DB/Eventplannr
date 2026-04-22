46d2529930462c5e140a098344b5703e64f2537f
Pepijn <pepijn@axyvon.com>
2026-04-22 20:39:43 +0200

    Biome fix

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -10,14 +10,18 @@ if (config.secret === "SECRET STRING SHOULD BE SET IN ENV") {
 
 database.connect().then((r) => console.log("Connected to database", r));
 
-setupDatabase().then((r) => {
-	if (r === null) return;
-	console.log("Database setup", r);
-}).catch((err) => {
-	console.error("\x1b[31m[ERROR] Error setting up database");
-	console.error(err);
-	console.error("Be aware that the database might not be set up correctly.\x1b[0m")
-});
+setupDatabase()
+	.then((r) => {
+		if (r === null) return;
+		console.log("Database setup", r);
+	})
+	.catch((err) => {
+		console.error("\x1b[31m[ERROR] Error setting up database");
+		console.error(err);
+		console.error(
+			"Be aware that the database might not be set up correctly.\x1b[0m",
+		);
+	});
 
 app.listen(config.port, () => {
 	console.log(`Server running on port ${config.port}`);
