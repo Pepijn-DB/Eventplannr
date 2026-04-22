bf6bef7809b55d41dc4fe5f9d792511f8118c9df
Pepijn <pepijn@axyvon.com>
2026-04-22 11:28:28 +0200

    Implement helper function for both database types

--- back-end/src/config/setupDatabase.ts
+++ back-end/src/config/setupDatabase.ts
@@ -1,3 +1,8 @@
+import * as db from "../services/databaseService.js"
+import {queryWithoutExecutioner} from "../services/databaseService.js";
+import {dbConfig} from "./config.js";
+import * as fs from 'node:fs';
+
 export async function setupDatabase():Promise<boolean> {
     try {
         const connected = await db.connect();
@@ -12,6 +17,29 @@ export async function setupDatabase():Promise<boolean> {
         return false;
     }
     throw new Error("Unsupported database type");
+}
+
+async function setupPostgres():Promise<boolean> {
+    const sql = `SELECT 1 FROM pg_database WHERE datname = '${dbConfig.database}'`;
+    const result = await queryWithoutExecutioner(sql);
+    if (result.rows.length !== 0) {
+        return true;
+    }
+    setupSql("./src/config/default_postgres_database.sql");
+
+    return true;
+}
+
+async function setupMySQL():Promise<boolean> {
+    const sql = `SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '${dbConfig.database}'`;
+    const result = await queryWithoutExecutioner(sql);
+    if (result.rows.length !== 0) {
+        return true;
+    }
+    setupSql("./src/config/default_sql_database.sql");
+    return true;
+}
+
 async function setupSql(filePath: string): Promise<void> {
     await queryWithoutExecutioner(`CREATE DATABASE ${dbConfig.database}`);
     fs.readFile(filePath, "utf-8", async (err, data) => {
