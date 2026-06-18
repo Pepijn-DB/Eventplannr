62f74715e28dee4d657226f40d5bf84ee0e646c3
Pepijn <pepijn@axyvon.com>
2026-06-18 23:20:15 +0200

    feat: create function for transactions

--- back-end/src/services/databaseService.ts
+++ back-end/src/services/databaseService.ts
@@ -131,6 +131,27 @@ export async function queryWithoutExecutioner(
 	}
 }
 
+export async function transaction<T>(
+	executioner: number,
+	callback: (
+		// biome-ignore lint/suspicious/noExplicitAny: <Result from SQL can return any>
+		txQuery: (sql: string, params: StrNum[]) => Promise<{ rows: any[] }>,
+	) => Promise<T>,
+): Promise<T> {
+	return db.begin(async (tx) => {
+		const txQuery = async (sql: string, params: StrNum[] = []) => {
+			const prepared = prepareQueryAndParams(sql, params);
+			const converted = convertQuestionMarksToDollarParams(prepared.sql);
+			const result = await tx.unsafe(converted.sql, prepared.params);
+			// biome-ignore lint/suspicious/noExplicitAny: <Result from SQL can return any>
+			const resultRows = Array.isArray(result) ? (result as any[]) : [];
+			await addLog(prepared.sql, executioner);
+			return { rows: resultRows };
+		};
+		return callback(txQuery);
+	});
+}
+
 export async function close(): Promise<void> {
 	db.close();
 	connected = false;
@@ -274,4 +295,11 @@ export function getDb() {
 	return db;
 }
 
-export default { getDb, connect, query, queryWithoutExecutioner, parseQuery };
+export default {
+	getDb,
+	connect,
+	query,
+	queryWithoutExecutioner,
+	transaction,
+	parseQuery,
+};
