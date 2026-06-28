ecaca56633748996c353842211eeefef0308fd02
Pepijn <pepijn@axyvon.com>
2026-06-29 01:39:00 +0200

    chore: add routesto the authController and excemt them from needing a token

--- back-end/src/routes/v1/authRoutes.ts
+++ back-end/src/routes/v1/authRoutes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { getToken } from "../../controllers/v1/authController.js";
+import {
+	getToken,
+	logout,
+	refreshToken,
+} from "../../controllers/v1/authController.js";
 import { validateBody } from "../../middlewares/validateRequest.js";
-import { authTokenSchema } from "../../schemas/index.js";
+import { authRefreshSchema, authTokenSchema } from "../../schemas/index.js";
 
 const router = Router();
 
 router.post("/token", validateBody(authTokenSchema), getToken);
+router.post("/refresh", validateBody(authRefreshSchema), refreshToken);
+router.post("/logout", validateBody(authRefreshSchema), logout);
 
 export default router;
